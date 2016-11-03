c63def00ff6e40b371f2d7775f4e508a0f4f24c2
root <napestershine@gmail.com>
2016-11-03 13:10:34 +0530

    created category test

---
+++ tests/CategoryTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class CategoryTest extends TestCase
+{
+    public function testCategoriesTable()
+    {
+        factory(App\Category::class)->create([
+            'name' => 'Movies',
+        ]);
+        $this->seeInDatabase('category', ['name'=> 'Movies']);
+    }
+}
