b39237ed5d130ef7dadb95448ea57482fe7b8ab5
root <napestershine@gmail.com>
2016-11-03 12:50:02 +0530

    updated migration

--- database/migrations/2016_11_03_071821_create_category_table.php
+++ database/migrations/2016_11_03_071821_create_category_table.php
@@ -15,6 +15,8 @@ class CreateCategoryTable extends Migration
     {
         Schema::create('category', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name')->unique();
+            $table->text('description');
             $table->timestamps();
         });
     }
