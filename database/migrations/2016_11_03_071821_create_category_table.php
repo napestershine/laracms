b2435a5892a6fb302d5e96f735efa7a6fa33b9ce
root <napestershine@gmail.com>
2016-11-03 13:33:48 +0530

    showed all category

--- database/migrations/2016_11_03_071821_create_category_table.php
+++ database/migrations/2016_11_03_071821_create_category_table.php
@@ -15,7 +15,7 @@ class CreateCategoryTable extends Migration
     {
         Schema::create('category', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name')->unique();
+            $table->string('name');
             $table->text('description');
             $table->timestamps();
         });
---
+++ resources/views/admin/category/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Categories</h1>
+    <ul>
+        @foreach($categories as $category)
+            <li>{{ $category->name }}</li>
+        @endforeach
+    </ul>
+@endsection
