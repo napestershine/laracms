eeb158f828a657bfaf68723e4f81cc99087936eb
root <napestershine@gmail.com>
2016-11-03 12:58:18 +0530

    categories  data unit test

--- tests/SeederTest.php
+++ tests/SeederTest.php
@@ -4,16 +4,10 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
+class SeederTest extends TestCase
 {
-    /**
-     * A basic functional test example.
-     *
-     * @return void
-     */
-    public function testBasicExample()
+    public function testCategoriesTable()
     {
-        $this->visit('/')
-             ->see('Laravel');
+        $this->seeInDatabase('cms', ['name'=> 'Movies']);
     }
 }
