2077a5da48f37370d8b76842bc60510ddffa6c13
root <napestershine@gmail.com>
2016-11-04 22:59:28 +0530

    added fillable

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $table = 'posts';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'description'
+    ];
 }
