b56d98d964ed0a09d57821912ef42c98c0393846
root <napestershine@gmail.com>
2016-11-04 21:46:42 +0530

    added content

---
+++ resources/views/category/new.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <h1>Create a category</h1>
+            @if (count($errors) > 0)
+                <div class="alert alert-danger">
+                    <strong>Whoops! Something went wrong!</strong>
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            <form action="/category" method="post">
+                {!! csrf_field() !!}
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" class="form-control" id="name" name="name" placeholder="Name">
+                </div>
+                <div class="form-group">
+                    <label for="description">Description</label>
+                    <textarea class="form-control" id="description" name="description"
+                              placeholder="description"></textarea>
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+        </div>
+    </div>
+@endsection
